bdfc8e1a04b91003a13c1b9013819c3691714a86
SayNo <81023511+Rynkana1478@users.noreply.github.com>
2021-12-21 23:44:41 +0700

    Update Raid.lua

--- Raid.lua
+++ Raid.lua
@@ -1,5 +1,6 @@
 repeat wait() until game:IsLoaded()
 wait(10)
+if not game.PlaceId == 2990100290 then
 syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/Raid.lua'))()]])
 setfflag("HumanoidParallelRemoveNoPhysics", "False")
 setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
@@ -51,4 +52,4 @@ pcall(function()
     end
 
 end)
-
+end
