996c348125f38f2c744d5e677d239bf816588397
SayNo <81023511+Rynkana1478@users.noreply.github.com>
2021-12-23 02:07:35 +0700

    Update Raid.lua

--- Raid.lua
+++ Raid.lua
@@ -9,6 +9,7 @@ setfflag("CrashPadUploadToBacktraceToBacktraceBaseUrl", "")
 setfflag("CrashUploadToBacktracePercentage", "0")
 setfflag("CrashUploadToBacktraceBlackholeToken", "")
 setfflag("CrashUploadToBacktraceWindowsPlayerToken", "")
+local vi = game:service'VirtualInputManager'
 local function NoclipLoop()
         if game.Players.LocalPlayer.Character ~= nil and _G.NoCIP == true then
             for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
@@ -27,30 +28,58 @@ _G.CNOCLIP = game:GetService('RunService').Stepped:Connect(NoclipLoop)
 end
 spawn(function()
     while _G.x do wait(1)
-        local args = {
-            [1] = "Skill",
-            [2] = "Skill1",
-            [3] = ""
-        }
-        local args1 = {
-            [1] = "Skill",
-            [2] = "Skill2",
-            [3] = "Attack"
-        }
-
-        game:GetService("ReplicatedStorage").Events.attack:FireServer(unpack(args))
-        wait(1)
-        game:GetService("ReplicatedStorage").Events.attack:FireServer(unpack(args1))
+        vi:SendKeyEvent(true, "Q", false, game)
+        vi:SendKeyEvent(true, "E", false, game)
+        vi:SendKeyEvent(true, "R", false, game)
     end
 end)
+function swing()
+    game:GetService("ReplicatedStorage").Events.attack:FireServer("Slash")
+    game:GetService("ReplicatedStorage").Events.attack:FireServer("T") 
+    game:GetService("ReplicatedStorage").Events.attack:FireServer("E")
+end
 pcall(function()
     while _G.x do wait()
         for i,v in pairs(game:GetService("Workspace").Mobs:GetChildren()) do
             repeat wait(.1)
                 togNoclip()
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame* CFrame.new(0,-11,0)* CFrame.Angles(math.rad(90), 0, 0)
-                game:GetService("ReplicatedStorage").Events.attack:FireServer("Slash")
-            until _G.x == false or v.Humanoid.health == 0
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame* CFrame.new(0,-9,0)* CFrame.Angles(math.rad(90), 0, 0)
+                swing()
+            until _G.x == false or v.Humanoid.health == 0 or not v
         end
+        local args = {
+                        [1] = "Sell",
+                        [2] = {
+                            [1] = {
+                                [1] = "21"
+                            },
+                            [2] = {
+                                [1] = "21"
+                            }
+                        }
+                    }
+                    local args2 = {
+                        [1] = "Sell",
+                        [2] = {
+                            [1] = {
+                                [1] = "A4"
+                            }
+                        }
+                    }
+                    local args3 = {
+                        [1] = "Sell",
+                        [2] = {
+                            [1] = {
+                                [1] = "22"
+                            }
+                        }
+                    }
+                    game:GetService("ReplicatedStorage").Events.drink:FireServer()
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.inventory:FireServer(unpack(args))
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.inventory:FireServer(unpack(args2))
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.inventory:FireServer(unpack(args3))
     end
 end)
