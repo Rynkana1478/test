4045b40078ce56e8fe56f46aefce4ecda939e702
SayNo <81023511+Rynkana1478@users.noreply.github.com>
2021-12-22 17:17:12 +0700

    Update Raid.lua

--- Raid.lua
+++ Raid.lua
@@ -1,22 +1,29 @@
 repeat wait() until game:IsLoaded()
 wait(10)
 syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/Raid.lua'))()]])
+_G.x = true
+_G.NoCIP = _G.x
 setfflag("HumanoidParallelRemoveNoPhysics", "False")
 setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
 setfflag("CrashPadUploadToBacktraceToBacktraceBaseUrl", "")
 setfflag("CrashUploadToBacktracePercentage", "0")
 setfflag("CrashUploadToBacktraceBlackholeToken", "")
 setfflag("CrashUploadToBacktraceWindowsPlayerToken", "")
-_G.x = true
-function noclip()
-    local hmn = game.Players.LocalPlayer.Character.Humanoid
-    _G.a = game:GetService("RunService").RenderStepped:connect(function()
-        if _G.x then
-                hmn:ChangeState(11)
-        else
-            _G.a:Disconnect()
+local function NoclipLoop()
+        if game.Players.LocalPlayer.Character ~= nil and _G.NoCIP == true then
+            for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                if child:IsA("BasePart") and child.CanCollide == true then
+                    child.CanCollide = false
+                    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+                end
+            end
         end
-    end)
+end
+local togNoclip = function()
+if _G.CNOCLIP then
+    _G.CNOCLIP:Disconnect()
+end
+_G.CNOCLIP = game:GetService('RunService').Stepped:Connect(NoclipLoop)
 end
 spawn(function()
     while _G.x do wait(1)
@@ -40,14 +47,10 @@ pcall(function()
     while _G.x do wait()
         for i,v in pairs(game:GetService("Workspace").Mobs:GetChildren()) do
             repeat wait(.1)
-                noclip()
+                togNoclip()
                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame* CFrame.new(0,-9,0)* CFrame.Angles(math.rad(90), 0, 0)
                 game:GetService("ReplicatedStorage").Events.attack:FireServer("Slash")
             until _G.x == false or v.Humanoid.health == 0
-            if type(v) == nil then
-                game:GetService("ReplicatedStorage").Events.drink:FireServer()
-            end
         end
     end
-
 end)
