9e867d50126fe17b2fd14bac60094c4b4e350533
SayNo <81023511+Rynkana1478@users.noreply.github.com>
2022-01-06 18:39:46 +0700

    Update Raid.lua

--- Raid.lua
+++ Raid.lua
@@ -1,8 +1,8 @@
 repeat wait() until game:IsLoaded()
 wait(10)
-syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/Raid.lua'))()]])
 _G.x = true
 _G.NoCIP = _G.x
+syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/Raid.lua'))()]])
 setfflag("HumanoidParallelRemoveNoPhysics", "False")
 setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
 setfflag("CrashPadUploadToBacktraceToBacktraceBaseUrl", "")
@@ -39,17 +39,18 @@ function swing()
     game:GetService("ReplicatedStorage").Events.attack:FireServer("E")
 end
 function totarget(Part,range)
-    local y = range + Part.Size.Y/2
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Part.CFrame* CFrame.new(0,-y,0)*CFrame.Angles(math.rad(90),0,0)
+    local y = range + Part.Size.Z/2
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Part.CFrame* CFrame.new(0,0,y)*CFrame.Angles(math.rad(0),0,0)
 end
 function farm()
     for i,v in pairs(game:GetService("Workspace").Mobs:GetChildren()) do
         if not v:FindFirstChild('Immune') then 
             repeat wait(.1)
-                totarget(v.hitbox,5)
+                totarget(v.hitbox,3)
                 togNoclip()
                 swing()
             until _G.x == false or v.Humanoid.health == 0 or v:FindFirstChild('Immune')
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame* CFrame.new(0,55,0)*CFrame.Angles(math.rad(0),0,0)
         end
         game:GetService("ReplicatedStorage").Events.drink:FireServer()
         local args = {
@@ -60,5 +61,5 @@ function farm()
     end
 end
 while wait() do wait(1)
-    farm()
+farm()
 end
