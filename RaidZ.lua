6b209062282dfdabc4b09e61037288d49227744e
SayNo <81023511+Rynkana1478@users.noreply.github.com>
2022-01-08 21:16:27 +0700

    Update RaidZ.lua

--- RaidZ.lua
+++ RaidZ.lua
@@ -4,7 +4,7 @@ for i,v in pairs(game:GetService("Workspace").Mobs:GetChildren()) do
 end
 _G.x = true
 _G.NoCIP = _G.x
-syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/Raid.lua'))()]])
+syn.queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Rynkana1478/test/main/RaidZ.lua'))()]])
 setfflag("HumanoidParallelRemoveNoPhysics", "False")
 setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
 setfflag("CrashPadUploadToBacktraceToBacktraceBaseUrl", "")
@@ -34,8 +34,8 @@ function swing()
     game:GetService("ReplicatedStorage").Events.attack:FireServer("E")
 end
 function totarget(Part,range)
-    local y = range + Part.Size.Y/2
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Part.CFrame* CFrame.new(0,-y,0)*CFrame.Angles(math.rad(90),0,0)
+    local y = range + Part.Size.Z/2
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Part.CFrame* CFrame.new(0,0,y)*CFrame.Angles(math.rad(0),0,0)
 end
 function farm()
     for i,v in pairs(game:GetService("Workspace").Mobs:GetChildren()) do
